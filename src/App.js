c93cbe81e69ec3dbea07ffe8532da7f641f7d585
ahmedhussin1 <ahmed208087@gmail.com>
2022-09-04 13:31:09 +0200

    creating todo list using react component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div>
+      <h1>My ToDo</h1>
+
+      <div className='card'>
+        <h2>TITLE</h2>
+        <div className="action">
+          <button className="delete-btn">DELETE</button>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
